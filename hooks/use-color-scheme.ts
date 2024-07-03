5531c46e100e3d4b778f47a6cbb2125cd81ca58f
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-02 23:42:36 -0600

    feat(use-color-scheme): return theme colors

--- hooks/use-color-scheme.ts
+++ hooks/use-color-scheme.ts
@@ -1,10 +1,18 @@
 import { useThemeStore } from "@/stores/use-theme-store";
 import { useColorScheme as useColorSchemeNative } from "react-native";
+import themeColors from "@/constants/colors";
 
 export function useColorScheme() {
   const systemTheme = useColorSchemeNative();
   const userTheme = useThemeStore((state) => state.theme);
 
   /* Return system theme if not user theme has been set */
-  return userTheme ? userTheme : systemTheme;
+  const theme = userTheme ? userTheme : systemTheme;
+  /* Return theme colors */
+  const colors = themeColors[theme ?? "light"];
+
+  return {
+    theme,
+    colors,
+  };
 }
