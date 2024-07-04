5785e33a341bd73974bd08c1eb133c7d68bb3fd2
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 13:56:58 -0600

    perf(symbol-list): use Flatlist to render elements

--- components/browse/symbol-list.tsx
+++ components/browse/symbol-list.tsx
@@ -1,31 +1,48 @@
 import { ThemedText } from "@/components/typography/themed-text";
 import { Symbol } from "@/types/symbol";
-import { TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
+import { memo } from "react";
+import { FlatList, TouchableOpacity, View } from "react-native";
 import Animated, { FadeIn, LinearTransition } from "react-native-reanimated";
 
 export function SymbolList({ data }: { data: Symbol[] }) {
   return (
     <View className="">
-      {data.map((symbol, i) => (
-        <Animated.View
-          entering={FadeIn.delay(i * 100)}
-          layout={LinearTransition}
-          key={symbol["1. symbol"]}
-          className="border-b-[1px] border-border p-5 rounded-lg"
-        >
-          <TouchableOpacity>
-            <View className="flex-row">
-              <View className="flex-1">
-                <ThemedText classNames="font-bold text-lg">
-                  {symbol["1. symbol"]}
-                </ThemedText>
-                <ThemedText intent={"subtle"}>{symbol["2. name"]}</ThemedText>
-              </View>
-              <ThemedText intent={"subtle"}>{symbol["3. type"]}</ThemedText>
-            </View>
-          </TouchableOpacity>
-        </Animated.View>
-      ))}
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => <SymbolItem symbol={item} i={index} />}
+        scrollEnabled={false}
+      />
+      {/*  {data.map((symbol, i) => (
+        
+      ))} */}
     </View>
   );
 }
+
+export function SymbolItem_({ symbol, i }: { symbol: Symbol; i: number }) {
+  return (
+    <Animated.View
+      entering={FadeIn.delay(i * 100)}
+      layout={LinearTransition}
+      key={symbol["1. symbol"]}
+      className="border-b-[1px] border-border p-5 rounded-lg"
+    >
+      <Link href={`/browse/${symbol["1. symbol"]}`} asChild>
+        <TouchableOpacity>
+          <View className="flex-row">
+            <View className="flex-1">
+              <ThemedText classNames="font-bold text-lg">
+                {symbol["1. symbol"]}
+              </ThemedText>
+              <ThemedText intent={"subtle"}>{symbol["2. name"]}</ThemedText>
+            </View>
+            <ThemedText intent={"subtle"}>{symbol["3. type"]}</ThemedText>
+          </View>
+        </TouchableOpacity>
+      </Link>
+    </Animated.View>
+  );
+}
+
+export const SymbolItem = memo(SymbolItem_);
