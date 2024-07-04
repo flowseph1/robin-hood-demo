da60ec42f45afebbe88522146839ea19c578cb12
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 12:06:39 -0600

    feat(animated-view): change entering and exiting animations

--- components/animated-view.tsx
+++ components/animated-view.tsx
@@ -4,12 +4,18 @@ import Animated, { FadeInDown, FadeOutUp } from "react-native-reanimated";
 export function AnimatedView({
   children,
   delay = 0,
+  classNames,
 }: {
   children: ReactNode;
   delay?: number;
+  classNames?: string;
 }) {
   return (
-    <Animated.View entering={FadeInDown.delay(delay)} exiting={FadeOutUp}>
+    <Animated.View
+      entering={FadeInDown.delay(delay)}
+      exiting={FadeOutUp}
+      className={classNames}
+    >
       {children}
     </Animated.View>
   );
