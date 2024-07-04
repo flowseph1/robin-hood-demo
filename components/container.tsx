2b61a3d82a6e52ea7e1343e039f8a37b1f4a79a4
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 10:11:06 -0600

    feat(container): add className attribute

--- components/container.tsx
+++ components/container.tsx
@@ -1,5 +1,14 @@
+import { cn } from "@/lib";
 import { View } from "react-native";
 
-export function Container({ children }: { children: React.ReactNode }) {
-  return <View className="flex-1 flex-col gap-2">{children}</View>;
+export function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <View className={cn("flex-1 flex-col gap-2", className)}>{children}</View>
+  );
 }
