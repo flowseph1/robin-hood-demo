89405071475fe64385bb49b0134350dda6e8fd32
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 02:40:04 -0600

    feat(home): add home list component

---
+++ components/home-lits.tsx
@@ -0,0 +1,46 @@
+import { Card } from "@/components/card";
+import { ThemedText } from "@/components/typography/themed-text";
+import { USDollar } from "@/lib/utils";
+import { View } from "react-native";
+
+const LIST = [
+  {
+    name: "Bitcoin",
+    coinName: "BTC",
+    price: 35350.1,
+  },
+  {
+    name: "Dogecoin",
+    coinName: "DOGE",
+    price: 0.234636,
+  },
+];
+
+export function HomeList() {
+  return (
+    <Card>
+      <View className="px-6 py-3 gap-7">
+        <ThemedText intent={"card-title"} classNames="pt-7">
+          Lists
+        </ThemedText>
+        {LIST.map((item) => (
+          <View
+            key={item.name}
+            className="flex-row items-center justify-between"
+          >
+            <View>
+              <ThemedText classNames="text-lg">{item.coinName}</ThemedText>
+              <ThemedText intent="subtle">{item.name}</ThemedText>
+            </View>
+
+            <View className="h-fit py-2 px-4 bg-primary rounded-lg items-center justify-center">
+              <ThemedText classNames="text-font-default text-md">
+                {USDollar.format(item.price)}
+              </ThemedText>
+            </View>
+          </View>
+        ))}
+      </View>
+    </Card>
+  );
+}
