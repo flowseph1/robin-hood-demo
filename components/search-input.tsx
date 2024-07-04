ab1f8c7cdcb92d7f9dadc03eef1de63e362afd7b
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 10:11:19 -0600

    feat(browse): add search input

---
+++ components/search-input.tsx
@@ -0,0 +1,40 @@
+import { useTheme } from "@/hooks/use-theme";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { TextInput, View } from "react-native";
+
+const PLACEHOLDER = "Search companies...";
+const TEXT_INPUT_HEIGHT = 40;
+
+export function SearchInput({
+  term,
+  setTerm,
+  handleFocus,
+}: {
+  term: string;
+  setTerm: (term: string) => void;
+  handleFocus: (value: boolean) => void;
+}) {
+  const { colors } = useTheme();
+
+  return (
+    <View className="flex-row gap-2 rounded-md bg-search-bar items-center px-3">
+      <FontAwesome5
+        name={"search"}
+        size={13}
+        color={colors["--color-gray-icon"]}
+      />
+      <TextInput
+        value={term}
+        onFocus={() => handleFocus(true)}
+        onBlur={() => handleFocus(false)}
+        onChangeText={setTerm}
+        placeholder={PLACEHOLDER}
+        className="w-full"
+        style={{
+          height: TEXT_INPUT_HEIGHT,
+          color: colors["--color-font-default"],
+        }}
+      />
+    </View>
+  );
+}
