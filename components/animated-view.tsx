5bd58792fcabb775b0931d7edaf11b2948dade3b
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 09:32:58 -0600

    feat(animated-view): implement animation

--- components/animated-view.tsx
+++ components/animated-view.tsx
@@ -1,7 +1,16 @@
-import { View } from "react-native";
-
 import { ReactNode } from "react";
+import Animated, { FadeInDown, FadeOutUp } from "react-native-reanimated";
 
-export function AnimatedView({ children }: { children: ReactNode }) {
-  return <View>{children}</View>;
+export function AnimatedView({
+  children,
+  delay = 0,
+}: {
+  children: ReactNode;
+  delay?: number;
+}) {
+  return (
+    <Animated.View entering={FadeInDown.delay(delay)} exiting={FadeOutUp}>
+      {children}
+    </Animated.View>
+  );
 }
