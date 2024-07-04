41f5fa949858a43a5644bd5b595c71edf2f9cba3
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 16:08:19 -0600

    feat(data): add new data

---
+++ data/global-quota.ts
@@ -0,0 +1,14 @@
+export const GLOBAL_QUOTE_DATA = {
+  "Global Quote": {
+    "01. symbol": "IBM",
+    "02. open": "177.8800",
+    "03. high": "177.9800",
+    "04. low": "175.1700",
+    "05. price": "175.7300",
+    "06. volume": "1649049",
+    "07. latest trading day": "2024-07-03",
+    "08. previous close": "177.3000",
+    "09. change": "-1.5700",
+    "10. change percent": "-0.8855%",
+  },
+};
