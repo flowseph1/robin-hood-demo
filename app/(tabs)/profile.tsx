04445bc0bb8d66499372e062fcdf0a0cbc3c641c
flowseph1 <josephthenewandarmonius@gmail.com>
2024-07-04 18:06:09 -0600

    feat(profile): add theme toggle button

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,20 @@
 import { ThemedText } from "@/components/typography/themed-text";
-import { View } from "react-native";
+import { useTheme } from "@/hooks/use-theme";
+import { TouchableOpacity, View } from "react-native";
 
 export default function ProfileScreen() {
+  const { theme, toggleColorScheme } = useTheme();
+
   return (
-    <View>
-      <ThemedText>Profile Screen</ThemedText>
+    <View className="flex-1 justify-center items-center gap-2">
+      <TouchableOpacity onPress={toggleColorScheme}>
+        <View className="">
+          <ThemedText intent={"primary"}>Toggle Theme</ThemedText>
+        </View>
+      </TouchableOpacity>
+      <ThemedText>
+        {theme === "dark" ? `Theme: Dark 🌙` : "Theme: Light 🌞"}
+      </ThemedText>
     </View>
   );
 }
